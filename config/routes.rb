cafa4a635f14ef7ac00d0c1d1c554f4795e259de
seagleax <litle.project@gmail.com>
2023-02-07 22:30:06 +0700

    remove unesesary asset

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   # USER ROUTES
-  get '/users', to: 'user#index'
+  get '/', to: 'user#index'
   get '/users/create', to: 'user#create'
   get '/users/edit', to: 'user#edit'
   get '/users/detail/:id', to: 'user#detail'
